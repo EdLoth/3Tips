157db6bf2ea45e1004bacc41c5c54c6aba77a8f4
Eduardo Ramos <eduardosram10@gmail.com>
2021-03-31 15:11:59 -0300

    Update

--- src/Pages/Play.tsx
+++ src/Pages/Play.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
-
 import styles from '../Styles/Pages/Play.module.css';
+import tips from '../tips.json'
 
 export default function Play() {
     return(
         <div className={styles.containerPlay}>
-            <div className="row">
-                <div className="col-12">
-                    <h1>3Dicas</h1>
-                </div>
-            </div>
+           <div className="container">
+               <h1>3 Dicas</h1>
+               <div className="questionContent">
+                   <p></p>
+               </div>
+               <div className="questionContent">
+                   dica 2 bla bla bla
+               </div>
+               <div className="questionContent">
+                   dica 3 bla bla bla
+               </div>
+               <div className="buttonGroup">
+                   <button>+1 Dica</button>
+                   <button>Correto</button>
+               </div>
+           </div>
         </div>
     )
 }
--- src/routes.tsx
+++ src/routes.tsx
@@ -9,6 +9,7 @@ import Home from './Pages/Home';
 import Index from './Pages/Index';
 import Login from './Pages/Login';
 import Register from './Pages/Register';
+import Play from './Pages/Play';
 
 
 export default function Routes() {
@@ -19,6 +20,7 @@ export default function Routes() {
                 <Route path="/index" component={Index}/>
                 <Route path="/Login" component={Login}/>
                 <Route path="/Register" component={Register} />
+                <Route path="/Play" component={Play}/>
             </Switch>
         </BrowserRouter>
     );
